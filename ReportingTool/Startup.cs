4ddc85f01a0f79c74e294c7049cf88f6fd9a2717
chenghuanliu <chenghuan.liu@woodplc.com>
2019-11-20 16:43:21 +0800

    mvcdefaultroute() ok. add db connectionstring

--- ReportingTool/Startup.cs
+++ ReportingTool/Startup.cs
@@ -16,6 +16,7 @@ namespace Reportingtool
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(option => option.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,6 +29,8 @@ namespace Reportingtool
             }
 
             app.UseStaticFiles();
+
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
