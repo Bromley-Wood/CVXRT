710cf90dbfcf37cb69933bc6a5391ed6769ca89b
chenghuanliu <chenghuan.liu@woodplc.com>
2019-11-21 14:44:28 +0800

    read action table test

---
+++ ReportingTool/Models/ActionsDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ReportingTool.Models
+{
+    public class ActionsDbContext : DbContext
+    {
+        public DbSet<Action> Actions { get; set; }
+
+        public ActionsDbContext(DbContextOptions<ActionsDbContext> options)
+            : base(options)
+        {
+            //this.EnsureSeedData();
+        }
+    }
+}
