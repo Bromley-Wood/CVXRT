a8649d59851d27cc56540292c0f5798cf089d55e
chenghuanliu <chenghuan.liu@woodplc.com>
2019-11-25 14:22:08 +0800

    show route call on schedule maintenance

---
+++ ReportingTool/Pages/ScheduleMaintenance.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ReportingTool.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Reportingtool.Pages
+{
+    public class ScheduleMaintenanceModel : PageModel
+    {
+        private readonly ReportingTool.Models.DatabaseContext _context;
+
+        public ScheduleMaintenanceModel(ReportingTool.Models.DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Route_Call> Route_Call { get; set; }
+        public async Task OnGetAsync()
+        {
+            Route_Call = await _context.Route_Call.ToListAsync();
+        }
+
+        
+        
+    }
+}
