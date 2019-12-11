88c0911d8f85a20511156044628128b34ecee4f2
chenghuanliu <chenghuan.liu@woodplc.com>
2019-12-11 11:27:23 +0800

    pass route call information to modal

--- ReportingTool/wwwroot/js/site.js
+++ ReportingTool/wwwroot/js/site.js
@@ -1,6 +1,18 @@
+
+// Pass the route call information to the edit modal
 $(document).on("click", ".btn-editform", function () {
+    //alert($(this).data('route'));
 
     var callid = $(this).data('callid');
-    $(".modal-body-edit #formEditRoute").val(callid);
-    alert(callid);
+    var route = $(this).data('route');
+    var cycledays = $(this).data('cycledays');
+    var labourhours = $(this).data('labourhours');
+    var plandate = $(this).data('plandate');
+
+    $(".modal-body-edit #formEditCallId").val(callid);
+    $(".modal-body-edit #formEditRoute").val(route);
+    $(".modal-body-edit #formEditCycle").val(cycledays);
+    $(".modal-body-edit #formEditLabourHours").val(labourhours);
+    $(".modal-body-edit #formEditPlannedDate").val(plandate);
+    
 });
