bfb0a8432cd2b2396482d13a7607654a6bba86db
chenghuanliu <chenghuan.liu@woodplc.com>
2019-12-19 10:29:58 +0800

    add username

--- ReportingTool/Pages/index.cshtml.cs
+++ ReportingTool/Pages/index.cshtml.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using ReportingTool.Models;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -14,6 +16,22 @@ namespace Reportingtool.Pages
         public IndexModel(ReportingTool.Models.DatabaseContext context)
         {
             _context = context;
+        }
+
+        public string Current_User { get; set; }
+        public async Task OnGetAsync()
+        {
+            Current_User = System.Security.Principal.WindowsIdentity.GetCurrent().Name.ToString();
+            string[] res = Current_User.Split("\\");
+            if (res.Length == 2)
+            {
+                Current_User = res[1];
+                Console.WriteLine("Current User Name is {0}", Current_User);
+            }
+            {
+                Current_User = "UnknownUser";
+                Console.WriteLine("Couldn't Get User Name. The name 'UnknownUser' is used instead");
+            }
         }
     }
 }
