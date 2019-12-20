b5e50eb5220c06d292b2685979111af1ffc2b4b4
chenghuanliu <chenghuan.liu@woodplc.com>
2019-12-20 12:31:50 +0800

    machine notes wip

---
+++ ReportingTool/Pages/MachineNotes.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ReportingTool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reportingtool.Pages
+{
+
+
+    public class MachineNotesModel : BasePageModel
+    {
+        private readonly ReportingTool.Models.DatabaseContext _context;
+
+        public MachineNotesModel(ReportingTool.Models.DatabaseContext context)
+        {
+            _context = context;
+        }
+
+    }
+}
