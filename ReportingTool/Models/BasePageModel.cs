5c560bee22fb8ff8e6f5f292c3a5662ea3e5d527
cheng <chenghuan.liu@woodplc.com>
2021-01-19 03:06:29 +0000

    modify base page model

--- ReportingTool/Models/BasePageModel.cs
+++ ReportingTool/Models/BasePageModel.cs
@@ -19,18 +19,9 @@ namespace Reportingtool.Pages
             if (Current_User == null){
                 Current_User = "admin";
             }
+           
             
-            string[] res = Current_User.Split("\\");
-            if (res.Length == 2)
-            {
-                Current_User = res[1];
-                Console.WriteLine("Current User Name is {0}", Current_User);
-            }
-            else
-            {
-                Current_User = "admin";
-                Console.WriteLine("Couldn't Get User Name. The name 'admin' is used instead");
-            }
+           
         }
     }
 }
